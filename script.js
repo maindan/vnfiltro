047f6a0ee045e41b34b22635a9044a6528660128
Daniel Lima <danielkksilva1@gmail.com>
2024-01-23 09:47:19 -0400

    alterações no javascript e scroll-cards add

--- script.js
+++ script.js
@@ -1,85 +1,34 @@
 
-const produtos = [
-    {
-        id: 1,
-        photo: 'public/products/compressor/12-100.png',
-        name: 'Compressor Schulz MSV 12 100L Isento',
-        price: 'R$ 9.000,00',
-    },
-    {
-        id: 2,
-        photo: 'public/products/compressor/20max.png',
-        name: 'Compressor Schulz 20 Max',
-        price: 'R$ 8.900,00',
-    },
-    {
-        id: 3,
-        photo: 'public/products/compressor/20sc.jpg',
-        name: 'Compressor Schulz 20 Audaz',
-        price: 'R$ 8.750,00',
-    },
-    {
-        id: 4,
-        photo: 'public/products/compressor/3015.png',
-        name: 'Compressor Schulz SRP 3015',
-        price: 'R$ 35.000,00',
-    },    
-];
-
-const productPhoto = document.getElementById('product-photo');
-const productName = document.getElementById('product-name');
-const price = document.getElementById('price');
-const buttonLeft = document.getElementById('button-left');
-const buttonRight = document.getElementById('button-right');
-
-let actualProduct = 0;
+const controls = document.querySelectorAll('.directional-button');
+const products = document.querySelectorAll('.product-details');
+const maxProducts = products.length;
+let actualProduct = 0
 
 
-function disabledLeftButton()  {
+
+controls.forEach((control) => {
     if(actualProduct == 0) {
-        buttonLeft.setAttribute('disabled', 'true');
-    };
-}
+        products[actualProduct].classList.add('actual-product');
+    }
+    control.addEventListener('click', (e) => {
+        isLeft = e.target.classList.contains('left');
+        isRight = e.target.classList.contains('right');
 
-function disabledRightButton() {
-    if(actualProduct == produtos.length - 1) {
-        buttonRight.setAttribute('disabled', 'true');
-    };
-}
+        if(isLeft && actualProduct != 0){
+            actualProduct -= 1;
+            console.log('esquerda', actualProduct);
+        };
+        if(isRight && actualProduct != maxProducts - 1){
+            actualProduct += 1;
+            console.log('direita', actualProduct);
+        }
 
-disabledLeftButton()
-disabledRightButton()
+        products.forEach((product) => product.classList.remove('actual-product'));
+        products[actualProduct].classList.add('actual-product');
 
-function defineProduct(index) {
-    productPhoto.setAttribute('src', produtos[index].photo);
-    productName.innerHTML = produtos[index].name;
-    price.innerHTML = produtos[index].price;
-}
-
-defineProduct(actualProduct);
-
-function nextProduct() {
-    if(actualProduct < produtos.length - 1) {
-        actualProduct = (actualProduct + 1) % produtos.length;
-        defineProduct(actualProduct);
-        disabledLeftButton()
-        disabledRightButton()
-        console.log(actualProduct);
-    };
-
-    if(actualProduct != 0) {
-        buttonLeft.removeAttribute('disabled');
-    };
-}
-
-function previousProduct() {
-    if(actualProduct != 0) {
-        actualProduct = (actualProduct - 1);
-        defineProduct(actualProduct);
-        disabledLeftButton();
-        console.log(actualProduct);
-    }; 
-    if(actualProduct != produtos.length - 1) {
-        buttonRight.removeAttribute('disabled');
-    };
-};
+        products[actualProduct].scrollIntoView({
+            behavior: "smooth",
+            inline: "center"
+        });
+    })
+});
