8f722a6ae47533a520564551dde8c03e040f1722
Daniel Lima <danielkksilva1@gmail.com>
2024-01-24 12:12:39 -0400

    adicionando função botão whatsapp

---
+++ wppbutton.js
@@ -0,0 +1,16 @@
+
+const wppBt = document.getElementById('wpp-button');
+const title = document.getElementById('section-title');
+const message = 'https://wa.me/5592993328100?text='
+
+function buttonLink(msg) {
+    document.getElementById('directLink').setAttribute('href', `${msg}`)
+}
+
+wppBt.addEventListener('click', () => {
+    const category = title.textContent.toLowerCase().split(' ')
+    const complementMsg = 'Tenho%20interesse%20%em%20%mais%20%informações%20sobre%20' + category[category.length - 1];
+    if(title.textContent.toLowerCase().includes('compressores')){
+        buttonLink(message + complementMsg);
+    };
+});
